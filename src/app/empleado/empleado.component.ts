38713afc6d5ce22d22edf9e0c7a252ee8864034b
mariana correa <correapintoluzmariana@gmail.com>
2023-06-07 17:50:13 -0500

    interpolacion empleado

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,10 +1,25 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
+  //poner css in line tambien se pude con html
+  //styles:["p{background-color:red;}"]
 })
 export class EmpleadoComponent {
+
+  nombre = "Mariana";
+
+  apellido = "Correa";
+
+  edad = "22"
+
+  empresa = "Meli"
+
+  //getEdad(){
+  //  return this.edad;
+  //}
 
 }
